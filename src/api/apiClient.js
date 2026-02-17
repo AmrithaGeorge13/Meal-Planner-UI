880cf4b1664596502c0cda75a3a0b579d46a59da
amrithag <amrithgeorge@gmail.com>
2026-02-17 08:58:59 +0530

    CRUD API CALL

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -8,3 +8,38 @@ export async function apiGet(endpoint){
     if(!json.success) throw new Error(json.message || 'request failed');
     return json.data;
 }
+
+
+export async function apiPost(endpoint,body){
+    const res = await fetch(`${API_BASE}${endpoint}`,{
+        method : 'POST',
+        headers: {'Content-Type':'application/json'},
+        body:JSON.stringify(body),
+    });
+    if(!res.ok) throw new Error(`HTTP ${res.status}`);
+    const json = await res.json();
+    if(!json.success) throw new Error(json.message || 'request failed');
+    return json.data;
+}
+
+export async function apiPut(endpoint,body){
+    const  res =  await fetch(`${API_BASE}${endpoint}`,{
+        method:'PUT',
+        headers:{'Content-Type':'application/json'},
+        body:JSON.stringify(body)
+    })
+    if(!res.ok) throw new Error(`HTTP ${res.status}`);
+    const json = await  res.json();
+    if(!json.success) throw new Error(json.message || 'request failed');
+    return json.data;
+}
+
+export async function apiDelete(endpoint){
+    const  res =  await fetch(`${API_BASE}${endpoint}`,{
+        method:'DELETE'
+    })
+    if(!res.ok) throw new Error(`HTTP ${res.status}`);
+    const json = await  res.json();
+    if(!json.success) throw new Error(json.message || 'request failed');
+    return json.data;
+}
--- src/api/households.js
+++ src/api/households.js
@@ -3,3 +3,18 @@ import {apiGet} from "./apiClient.js";
 export async function getHouseholds(){
     return apiGet('/households');
 }
+
+export async function getHousehold(id){
+    return apiGet(`/households/${id}`);
+}
+
+export async function createHousehold(body){
+    return apiGet(`/households`,body);
+}
+export async function updateHousehold(id,body){
+    return apiGet(`/households/${id}`,body);
+}
+
+export async function deleteHousehold(id){
+    return apiGet(`/households/${id}`);
+}
